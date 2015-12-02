893614fa8862b08f46a887c40e500a97e47b29cf
CUG-Onion <cug_yangyuancong@hotmail.com>
2015-12-02 18:16:11 +0800

    Update UTF-8 2 GBK.sh

--- UTF-8 2 GBK.sh
+++ UTF-8 2 GBK.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 Depth=10
-find .  -maxdepth "$Depth"  -type f -exec bash -c 'grep -Iq . "{}"' \; -and -print0 | xargs -0 -n 1   bash -c 'file --mime "$@" | grep   "charset=utf-8"| cut -d : -f 1 ;' bash | xargs -d '\n' -n 1  bash -c  'iconv -c -f UTF-8 -t  GBK "$@" -o "$@"; echo "Converting file$@ .........................Finished";' bash
+find .  -maxdepth "$Depth"  -type f -exec bash -c 'grep -Iq . "{}"' \; -and -print0 | xargs -0 -n 1   bash -c 'file --mime "$@" | grep   "charset=utf-8"| cut -d : -f 1 | grep -v  ".txt\|.bat\|.sh\|.eps" ;' bash | xargs -d '\n' -n 1  bash -c  'iconv -c -f UTF-8 -t  GBK "$@" -o "$@"; echo "Converting file$@ .........................Finished";' bash
